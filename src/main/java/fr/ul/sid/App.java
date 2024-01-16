9648a2842fbd818c4684aea2da68642a38314cf7
Athlaes <chartierlucas@outlook.fr>
2024-01-16 16:19:07 +0100

    First commit

---
+++ src/main/java/fr/ul/sid/App.java
@@ -0,0 +1,13 @@
+package fr.ul.sid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
